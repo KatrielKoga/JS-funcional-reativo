5e856ae86bf63cb78deb72c812d100440ec0cc02
KatrielKoga <katriel.koga.2012@hotmail.com>
2023-10-14 16:53:52 -0300

    functional JS `first class function`

---
+++ funcional/first_class_func.js
@@ -0,0 +1,8 @@
+// Diz se que uma linguagem de programação possui
+// funções de primeira classe quando funções nessa
+// linguagem são tratadas como qualquer outra variável
+
+const x = 3;
+const y = function (txt) {
+	console.log(txt);
+};
